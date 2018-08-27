a856b7cdf98202589b3041b73621235411f75795
James Sleeman <james@gogo.co.nz>
2018-08-27 14:04:39 +1200

    Update docs

--- src/SimpleSleep.h
+++ src/SimpleSleep.h
@@ -7,16 +7,17 @@
 
 /** Simple Sleep class for Arduino.
  * 
- * SimpleSleep Sleep;
+ *     SimpleSleep Sleep;
  * 
- * Sleep.forever(); // Deep dreamless slumber, bod off, adc off, interrupts off
- * Sleep.deeply();  // Wakes up on external interrupts, Power Save mode or Power Down mode, bod off, adc off
- * Sleep.lightly(); // Wakes up on externl interrupts, some internal interrupts, timers generally off, adc off
- * Sleep.idle();    // Wakes up on most anything, timers still run, adc is on
+ *     Sleep.forever(); // Deep dreamless slumber, bod off, adc off, interrupts off
  * 
- * Sleep.deeplyFor(arbitraryMs);
- * Sleep.lightlyFor(arbitraryMs);
- * Sleep.idleFor(arbitraryMs);
+ *     Sleep.deeply();  // Wakes up on external interrupts, Power Save mode or Power Down mode, bod off, adc off
+ *     Sleep.lightly(); // Wakes up on externl interrupts, some internal interrupts, timers generally off, adc off
+ *     Sleep.idle();    // Wakes up on most anything, timers still run, adc is on
+ * 
+ *     Sleep.deeplyFor(arbitraryMs);
+ *     Sleep.lightlyFor(arbitraryMs);
+ *     Sleep.idleFor(arbitraryMs);
  * 
  */
 
@@ -27,7 +28,8 @@
      /** Sleep forever in a deep and dreamless slumber.  
       * 
       *  Everything that can be shut down is shut down, not even interrupts will wake you.  
-      *  The only way to recover from forever() is to reset. 
+      * 
+      *  The only way to recover from `forever()` is to reset. 
       * 
       *  This is a one-way operation, minimum power consumption.
       *
@@ -41,6 +43,14 @@
        */
     
       inline void deeply()                    { sleepDeeply(0);       }
+            
+      /** Sleep deeply for a given time, allow external interrupts where possible (LEVEL only usually), bod off, adc off, timers generally off
+       * 
+       * Will not return until the time has elapsed, think of this as a low power `delay()`.
+       * 
+       * For AVR, typically implemented as Power Down
+       */
+      
       inline void deeplyFor(uint32_t sleepMs) { sleepDeeply(sleepMs); }
       
       
@@ -50,14 +60,34 @@
        */
       
       inline void lightly()                    { sleepLightly(0); }
+      
+      /** Sleep lightly for a given time, allow many interrupts, adc off, timers generally off
+       * 
+       * Will not return until the time has elapsed, think of this as a low power `delay()`.
+       * 
+       *  For AVR, typically either implemented as Extended Standby or ADC Noise Reduction with the ADC **OFF**.
+       */
+      
       inline void lightlyFor(uint32_t sleepMs) { sleepLightly(sleepMs); }
       
       /** Wait patiently, most anything can wake you including Serial, timers etc. 
        *
-       * For AVR, typically implemented as Idle
+       *  Most importantly, millis() and Serial will continue to function.
+       * 
+       *  For AVR, typically implemented as Idle
        */
       
       inline void idle()                        { sleepIdle(0);       }
+      
+      /** Wait patiently for a given time.
+       * 
+       *  Most importantly, millis() and Serial will continue to function.
+       *
+       *  Will not return until the time has elapsed, think of this as a low power `delay()`.
+       * 
+       *  For AVR, typically implemented as Idle
+       */
+      
       inline void idleFor(uint32_t sleepMs)     { sleepIdle(sleepMs); }
       
       /** For more accurate sleep times, you can generate calibration data and pass
@@ -71,8 +101,26 @@
        */
       
       SimpleSleep_Cal getCalibration();
+      
+      /** Sleep deeply for a given time with a pre-determined calibration factor. 
+       *
+       * Use `getCalibration()` to obtain the calibration data.
+       */
+      
       void deeplyFor(uint32_t sleepMs, SimpleSleep_Cal calibrationData);
+      
+      /** Sleep lightly for a given time with a pre-determined calibration factor. 
+       *
+       * Use `getCalibration()` to obtain the calibration data.
+       */
+      
       void lightlyFor(uint32_t sleepMs, SimpleSleep_Cal calibrationData);
+      
+      /** Idle for a given time with a pre-determined calibration factor. 
+       *
+       * Use `getCalibration()` to obtain the calibration data.
+       */
+      
       void idleFor(uint32_t sleepMs, SimpleSleep_Cal calibrationData);
       
       
