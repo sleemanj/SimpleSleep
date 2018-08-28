e5c029a1a2b42bf156cb681ab049ff2bebf4995c
James Sleeman <james@gogo.co.nz>
2018-08-28 12:11:47 +1200

    Update doxygen docs

--- docs/html/class_simple_sleep.js
+++ docs/html/class_simple_sleep.js
@@ -11,8 +11,11 @@ var class_simple_sleep =
     [ "lightly", "class_simple_sleep.html#aed484ecdc2f5a8b282358cde120fe17b", null ],
     [ "lightlyFor", "class_simple_sleep.html#afb9814f6aba4fec62b03c44f902590e5", null ],
     [ "lightlyFor", "class_simple_sleep.html#a2c2ef04257b7e0d1c59897464cf287f1", null ],
+    [ "sleepDeeply", "class_simple_sleep.html#a2d45cc2ee96a62c4730557d367748893", null ],
     [ "sleepDeeply", "class_simple_sleep.html#a0b2d6548ce7a30772f8abf3bd89c27e3", null ],
     [ "sleepForever", "class_simple_sleep.html#ae093c6588e675db2819c15b2e80b379b", null ],
+    [ "sleepIdle", "class_simple_sleep.html#ab5c6753e39db462968055746393d6431", null ],
     [ "sleepIdle", "class_simple_sleep.html#a0098c9c765694c1de606bcc1cdf4a2d6", null ],
+    [ "sleepLightly", "class_simple_sleep.html#a0eed48602c3d639c5f4ed7a68bef18e7", null ],
     [ "sleepLightly", "class_simple_sleep.html#a755acf21f6e8a2bb52c23e0ca7a2dbe1", null ]
 ];
